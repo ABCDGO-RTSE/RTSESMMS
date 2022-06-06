efbb2c6295bc20a54bc7bf52996f68dabe59a971
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2022-06-06 12:40:17 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - rtse_soil_system',
-    title: 'rtse_soil_system',
+    titleTemplate: '%s - Soil Moisture Monitoring Sesnsor',
+    title: 'Soil Moisture Monitoring Sesnsor',
     htmlAttrs: {
       lang: 'en'
     },
